733775ee5cea7ab53d461b4e35831aca9110b213
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-10 17:52:23 -0400

    Fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React from 'react';
 import './Calculator.css';
 
@@ -8,27 +7,27 @@ const Calculator = () => (
       <div className="output">0</div>
       <div className="buttons-container">
         <div className="graybg grayBtn">
-          <button>AC</button>
-          <button>+/-</button>
-          <button>%</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button className="span-two">0</button>
-          <button>.</button>
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="span-two">0</button>
+          <button type="button">.</button>
         </div>
         <div className="orangebg orangeBtn">
-          <button>÷</button>
-          <button>x</button>
-          <button>-</button>
-          <button>+</button>
-          <button>=</button>
+          <button type="button">÷</button>
+          <button type="button">x</button>
+          <button type="button">-</button>
+          <button type="button">+</button>
+          <button type="button">=</button>
         </div>
       </div>
     </div>
