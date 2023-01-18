4750adcf31774c645a88afbae2e6f20295219697
shinhyo26okazaki <shibo6937@gmail.com>
2023-01-18 18:32:15 -0400

    Add testing for calculator component

--- src/components/__test__/Calculator.test.js
+++ src/components/__test__/Calculator.test.js
@@ -1,7 +1,34 @@
+import React from "react";
 import Calculator from "../Calculator";
+import renderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 describe('Calculator component', () => {
-  test('', () => {
-    
+
+  test('Test Calculator render correctly', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Correctly calculate ÷ of 2 numbers', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('÷'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('='));
+    const output = screen.getByTestId('displayOutput');
+    expect(output.innerHTML).toBe('5');
+  });
+
+  test('Correctly calculate + of 2 numbers', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('='));
+    const output = screen.getByTestId('displayOutput');
+    expect(output.innerHTML).toBe('30');
   });
 });
